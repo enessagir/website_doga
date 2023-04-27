59dad9918840c2cc705b84d2f514af5ff15d7f37
Enes <ensagir@gmail.com>
2023-04-27 12:35:34 +0300

    Initial commit

---
+++ templates/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("carousel-item");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 5000);
+  }
+});
